a0a110e529304ff53a7651c1ccde58a65e55eac3
Gor <7495500@gmail.com>
2020-07-06 00:50:26 +0300

    Add bootstrap styles

--- app/controllers/technologies_controller.rb
+++ app/controllers/technologies_controller.rb
@@ -6,4 +6,43 @@ class TechnologiesController < ApplicationController
   def show
     @technology = Technology.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @technology = Technology.new(technology_params)
+
+    if @technology.save
+      redirect_to @technology
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @technology = Technology.find(params[:id])
+  end
+
+  def update
+    @technology = Technology.find(params[:id])
+
+    if @technology.update(technology_params)
+      redirect_to @technology
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @technology = Technology.find(params[:id])
+    @technology.destroy
+
+    redirect_to technologies_path
+  end
+
+  private
+  def technology_path
+    params.require(:technology).permit(:name, :purpose, :description, :popularity)
+  end
 end
